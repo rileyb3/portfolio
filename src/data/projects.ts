ee2e1d6aed1c60269d3a46a4b76667a36cbafa8f
Riley Byers <rileyabyers@gmail.com>
2026-09-04 11:21:16 -0400

    Fix green-wall photo caching (renamed again after in-place edit), drop paint swatch photo from slideshow

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -455,10 +455,9 @@ Deno.serve(async (req) => {
           {
             type: "slideshow",
             images: [
-              "/projects/childhood-bedroom/paint-swatches.jpg",
               "/projects/childhood-bedroom/mural-wide-v2.jpg",
               "/projects/childhood-bedroom/mural-close-v2.jpg",
-              "/projects/childhood-bedroom/green-wall-v2.jpg",
+              "/projects/childhood-bedroom/green-wall-final.jpg",
             ],
           },
           {
